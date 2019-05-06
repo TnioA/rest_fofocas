d7210da8ce94143bb20d5661635296026364c23e
tnioa <hortanio@hotmail.com>
2019-05-05 21:16:48 -0300

    inclusao

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def getfofocas():
 	return jsonify({'destaques' : data, 'fofocas' : fofocas, 'ultimas' : ultimas_noticias})
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
